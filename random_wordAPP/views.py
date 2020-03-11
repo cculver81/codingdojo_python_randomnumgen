1e39fc741b561f90ca28e57f4beefac022edcc3a
Cinque Culver <culver.ce@gmail.com>
2020-03-11 16:34:55 -0400

    Second sanity check complete, random_word is print through context dictionary to index.html

--- random_wordAPP/views.py
+++ random_wordAPP/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from django.utils.crypto import get_random_string
 
 def index(request):
-    return HttpResponse('This is a test')
+    context = {
+        'random_string' : get_random_string(length=14)
+    }
+    return render(request, 'index.html', context)
